87f9290d5731cbe3fe4aded47982af7167450230
aerdy <aerdy4@gmail.com>
2014-08-11 11:32:18 +0700

    Turunana
    Turunan

---
+++ src/RiwayatHidup.java
@@ -0,0 +1,15 @@
+
+public class RiwayatHidup {
+	//kita akan memulai
+	private String nama;
+	private long lahir;
+	private int umur;
+	
+	public void setNama(String nama2){
+		this.nama=nama2;
+	}
+	public String getNama(){
+		return this.nama;
+	}
+
+}
